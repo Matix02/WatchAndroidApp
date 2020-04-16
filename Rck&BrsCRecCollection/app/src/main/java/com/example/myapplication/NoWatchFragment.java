456aaf416a34f1c3e5a8002c7a7af8857a4c05fa
Matix02 <48627951+Matix02@users.noreply.github.com>
2020-04-16 12:15:45 +0200

    Update of Listing Data in MainView

--- Rck&BrsCRecCollection/app/src/main/java/com/example/myapplication/NoWatchFragment.java
+++ Rck&BrsCRecCollection/app/src/main/java/com/example/myapplication/NoWatchFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
@@ -24,6 +26,9 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+import static android.content.ContentValues.TAG;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -43,35 +48,58 @@ public class NoWatchFragment extends WatchingState{
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        reff = FirebaseDatabase.getInstance().getReference().child("Element");
+        ValueEventListener eventListener =  new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    // L1.add(String.valueOf(postSnapshot.getKey()));
+                    String d = (String) postSnapshot.child("title").getValue();
+                    String e = (String) postSnapshot.child("category").getValue();
+                    // L2.add(Objects.requireNonNull(dataSnapshot.getValue(Element.class)).getTitle());
+                    //  System.out.println(L2);
+                    // L1.add(reff.child(d).child("title"));
+                    L1.add(d);
+                    L2.add(e);
+                }
+                Log.d("TAG", String.valueOf(L2));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        };
+        reff.addValueEventListener(eventListener);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        L1.add("Once upon a hollywood");
-        L1.add("Metro Exdodus");
-        L2.add("Film");
-        L2.add("Gra");
         reff = FirebaseDatabase.getInstance().getReference().child("Element");
-        reff.addValueEventListener(new ValueEventListener() {
+        ValueEventListener eventListener =  new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                   // L1.add(String.valueOf(postSnapshot.getKey()));
-                    L2.add(Objects.requireNonNull(dataSnapshot.getValue(Element.class)).getTitle());
-                    System.out.println(L2);
+                    // L1.add(String.valueOf(postSnapshot.getKey()));
+                    String d = (String) postSnapshot.child("title").getValue();
+                    String e = (String) postSnapshot.child("category").getValue();
+                    // L2.add(Objects.requireNonNull(dataSnapshot.getValue(Element.class)).getTitle());
+                    //  System.out.println(L2);
+                    // L1.add(reff.child(d).child("title"));
+                    L1.add(d);
+                    L2.add(e);
                 }
+                Log.d("TAG", String.valueOf(L2));
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {}
-        });
+        };
+        reff.addValueEventListener(eventListener);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
 
         if(view == null)
             view = inflater.inflate(R.layout.fragment_no_watch, container, false);
@@ -79,6 +107,30 @@ public class NoWatchFragment extends WatchingState{
             ViewGroup parent = (ViewGroup) view.getParent();
             parent.removeView(view);
         }
+        L1.add("dasdasd");
+        L2.add("dasdasd");
+        reff = FirebaseDatabase.getInstance().getReference().child("Element");
+
+reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    // L1.add(String.valueOf(postSnapshot.getKey()));
+                    String d = (String) postSnapshot.child("title").getValue();
+                    String e = (String) postSnapshot.child("category").getValue();
+                    // L2.add(Objects.requireNonNull(dataSnapshot.getValue(Element.class)).getTitle());
+                    //  System.out.println(L2);
+                    // L1.add(reff.child(d).child("title"));
+                    L1.add(d);
+                    L2.add(e);
+                }
+                Log.d("TAG", String.valueOf(L2));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException()); }
+        });
+//        reff.addValueEventListener(eventListener);
         ListAdapter adapter = new ListAdapter(getActivity(), R.layout.fragment_no_watch, L1, L2);
         ListView listView = (ListView) view.findViewById(R.id.exp_no_listview);
 
